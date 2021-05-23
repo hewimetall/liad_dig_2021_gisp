1f6f1c9d2f35f6053416691a1c47966277720089
Ваше Имя <you@example.com>
2021-05-23 14:59:26 +0800

    fix

--- src/core/events.py
+++ src/core/events.py
@@ -6,10 +6,11 @@ from fastapi.staticfiles import StaticFiles
 
 from db.events import init_app
 from db.run_load_data import save_data
-
+from ml.main import DataProcess
 
 def create_start_app_handler(app: FastAPI) -> Callable:  # type: ignore
     async def start_app() -> None:
         app.mount("/static", StaticFiles(directory="static"), name="static")
+        DataProcess()
         await init_app(app)
     return start_app
